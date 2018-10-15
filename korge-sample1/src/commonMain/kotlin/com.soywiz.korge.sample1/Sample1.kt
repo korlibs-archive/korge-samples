e5b023800c76968e7d07cf3d0b2be614a79e44e9
soywiz <soywiz@gmail.com>
2018-10-15 04:40:32 +0200

    Small adjustments

--- korge-sample1/src/commonMain/kotlin/com.soywiz.korge.sample1/Sample1.kt
+++ korge-sample1/src/commonMain/kotlin/com.soywiz.korge.sample1/Sample1.kt
@@ -1,9 +1,18 @@
 package com.soywiz.korge.sample1
 
-import com.soywiz.korge.Korge
-import com.soywiz.korge.view.solidRect
-import com.soywiz.korim.color.Colors
+import com.soywiz.klock.*
+import com.soywiz.korge.*
+import com.soywiz.korge.tween.*
+import com.soywiz.korge.view.*
+import com.soywiz.korim.color.*
+import com.soywiz.korio.async.*
 
 fun main(args: Array<String>) = Korge {
-	solidRect(100, 100, Colors.RED)
+	val rect = solidRect(100, 100, Colors.RED)
+	launchImmediately {
+		while (true) {
+			rect.tween(rect::x[100], time = 1.seconds, easing = Easing.EASE_IN_OUT_QUAD)
+			rect.tween(rect::x[0], time = 1.seconds, easing = Easing.EASE_IN_OUT_QUAD)
+		}
+	}
 }
