407788d621909ebae9d6ebfc202446eb15c2d31a
soywiz <soywiz@gmail.com>
2018-10-15 04:37:22 +0200

    nmpp

---
+++ korge-sample1/src/commonMain/kotlin/com.soywiz.korge.sample1/Sample1.kt
@@ -0,0 +1,9 @@
+package com.soywiz.korge.sample1
+
+import com.soywiz.korge.Korge
+import com.soywiz.korge.view.solidRect
+import com.soywiz.korim.color.Colors
+
+fun main(args: Array<String>) = Korge {
+	solidRect(100, 100, Colors.RED)
+}
