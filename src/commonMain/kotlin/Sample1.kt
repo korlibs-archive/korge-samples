63c90df48ec1cdb6b8719236b0ced9df14bc2411
soywiz <soywiz@gmail.com>
2018-12-23 00:09:38 +0100

    Box2d sample

--- src/commonMain/kotlin/Sample1.kt
+++ src/commonMain/kotlin/Sample1.kt
@@ -1,10 +1,13 @@
 import com.soywiz.korge.*
 import com.soywiz.korge.box2d.*
+import com.soywiz.korge.scene.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.*
+import org.jbox2d.collision.shapes.*
 import org.jbox2d.dynamics.*
 
 fun main() = Korge {
+	//sceneContainer(views)
 	//solidRect(300, 200, Colors.DARKCYAN)
 	graphics {
 		fill(Colors.DARKCYAN) {
@@ -19,24 +22,19 @@ fun main() = Korge {
 		position(100, 100)
 	}
 	worldView {
-		position(200, 200).scale(20)
+		position(400, 400).scale(20)
 
 		createBody {
 			setPosition(0, -10)
 		}.fixture {
 			shape = BoxShape(100, 20)
 			density = 0f
-		}.setView(graphics {
-			fill(Colors.RED) {
-				drawRect(-50f, -10f, 100f, 20f)
-				//anchor(0.5, 0.5)
-			}
-		})
+		}.setViewWithContainer(solidRect(100, 20, Colors.RED).position(-50, -10))
 
 		// Dynamic Body
 		createBody {
 			type = BodyType.DYNAMIC
-			setPosition(0, 10)
+			setPosition(0, 7)
 		}.fixture {
 			shape = BoxShape(2f, 2f)
 			density = 0.5f
@@ -45,7 +43,7 @@ fun main() = Korge {
 
 		createBody {
 			type = BodyType.DYNAMIC
-			setPosition(0.75, 15)
+			setPosition(0.75, 13)
 		}.fixture {
 			shape = BoxShape(2f, 2f)
 			density = 1f
@@ -54,6 +52,23 @@ fun main() = Korge {
 			fill(Colors.BLUE) {
 				drawRect(-1f, -1f, 2f, 2f)
 			}
+		})
+
+		createBody {
+			type = BodyType.DYNAMIC
+			setPosition(0.5, 15)
+		}.fixture {
+			shape = CircleShape().apply { m_radius = 2f }
+			density = 22f
+			friction = 3f
+		}.setView(graphics {
+			fill(Colors.BLUE) {
+				drawCircle(0, 0, 200)
+			}
+			fill(Colors.DARKCYAN) {
+				drawCircle(100, 100, 20)
+			}
+			scale(1f / 100f)
 		})
 	}
 }
--- src/commonTest/kotlin/Sample1Test.kt
+++ src/commonTest/kotlin/Sample1Test.kt
@@ -1,4 +1,3 @@
-/*
 import com.soywiz.korge.tests.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.*
@@ -6,7 +5,8 @@ import kotlin.test.*
 
 class Sample1Test : KorgeTest() {
 	@Test
-	fun test() = viewsTest {
+	fun test() {
+		/*
 		views.stage.apply {
 			val rect1 = solidRect(100, 100, Colors.AQUAMARINE)
 			val rect2 = solidRect(100, 100, Colors.AQUAMARINE).position(-200, -200)
@@ -14,6 +14,7 @@ class Sample1Test : KorgeTest() {
 			assertEquals(true, rect1.isVisibleToUser())
 			assertEquals(false, rect2.isVisibleToUser())
 		}
+		*/
+		assertEquals(true, true)
 	}
 }
-*/
