99930419a55c85cc9cabf1319ad438df8e350587
soywiz <soywiz@gmail.com>
2019-03-06 21:00:01 +0100

    Update 3d sample

--- sample-raw-3d/src/commonMain/kotlin/main.kt
+++ sample-raw-3d/src/commonMain/kotlin/main.kt
@@ -1,4 +1,3 @@
-import com.soywiz.kds.iterators.*
 import com.soywiz.klock.*
 import com.soywiz.korge.*
 import com.soywiz.korge.experimental.s3d.*
@@ -13,19 +12,20 @@ suspend fun main() = Korge(title = "KorGE 3D") {
 	image(resourcesVfs["korge.png"].readNativeImage()).alpha(0.5)
 
 	scene3D {
-		camera.set(fov = 60.degrees, near = 0.3, far = 1000.0)
+		//camera.set(fov = 60.degrees, near = 0.3, far = 1000.0)
 
-		val cube = box {
-		}
-		val cube2 = box {
-			position(0, 2, 0)
-			scale(1, 2, 1)
-			rotation(0.degrees, 0.degrees, 45.degrees)
-		}
+		val cube1 = box()
+		val cube2 = box().position(0, 2, 0).scale(1, 2, 1).rotation(0.degrees, 0.degrees, 45.degrees)
+		val cube3 = box().position(-5, 0, 0)
+		val cube4 = box().position(+5, 0, 0)
+		val cube5 = box().position(0, -5, 0)
+		val cube6 = box().position(0, +5, 0)
+		val cube7 = box().position(0, 0, -5)
+		val cube8 = box().position(0, 0, +5)
 
 		var tick = 0
 		addUpdatable {
-			val angle = (tick / 2.0).degrees
+			val angle = (tick / 4.0).degrees
 			camera.positionLookingAt(
 				cos(angle * 2) * 4, cos(angle * 3) * 4, -sin(angle) * 4, // Orbiting camera
 				0, 1, 0
@@ -36,12 +36,12 @@ suspend fun main() = Korge(title = "KorGE 3D") {
 		launchImmediately {
 			while (true) {
 				tween(time = 16.seconds) {
-					cube.modelMat.identity().rotate((it * 360).degrees, 0.degrees, 0.degrees)
+					cube1.modelMat.identity().rotate((it * 360).degrees, 0.degrees, 0.degrees)
 					cube2.modelMat.identity().rotate(0.degrees, (it * 360).degrees, 0.degrees)
 				}
 			}
 		}
 	}
 
-	image(resourcesVfs["korge.png"].readNativeImage()).position(1280 - 512, 0).alpha(1).alpha(0.5)
+	image(resourcesVfs["korge.png"].readNativeImage()).position(views.virtualWidth, 0).anchor(1, 0).alpha(0.5)
 }
