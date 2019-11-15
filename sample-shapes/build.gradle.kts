3d3a9c6b84157d3f4a1583873d0673a2aa93a6d3
soywiz <soywiz@gmail.com>
2019-11-15 19:26:13 +0100

    Added sample showing how to use Graphics to create a circle view

---
+++ sample-shapes/build.gradle.kts
@@ -0,0 +1,11 @@
+import com.soywiz.korge.gradle.*
+
+apply(plugin = "korge")
+
+korge {
+	id = "com.soywiz.sample1"
+	name = "Sample1"
+	description = "A sample using Korge and the gradle plugin"
+	orientation = com.soywiz.korge.gradle.Orientation.LANDSCAPE
+	jvmMainClassName = "Sample1Kt"
+}
---
+++ sample-shapes/src/commonMain/kotlin/Sample1.kt
@@ -0,0 +1,53 @@
+import com.soywiz.klock.*
+import com.soywiz.korge.*
+import com.soywiz.korge.view.*
+import com.soywiz.korgw.*
+import com.soywiz.korim.color.*
+import com.soywiz.korio.async.*
+import com.soywiz.korma.geom.vector.*
+
+suspend fun main() = Korge(quality = GameWindow.Quality.PERFORMANCE, title = "My Awesome Box2D Game!") {
+	setupCircle()
+}
+
+fun Stage.setupCircle() {
+	val circle = Circle(radius = 32.0)
+	addChild(circle)
+	launch {
+		while (true) {
+			circle.x++
+			circle.y++
+			circle.radius++
+			delay(16.milliseconds)
+		}
+	}
+}
+
+class Circle(radius: Double = 16.0, color: RGBA = Colors.WHITE) : Container() {
+	var radius: Double = radius
+		set(value) {
+			field = value; updateGraphics()
+		}
+
+	var color: RGBA = color
+		set(value) {
+			field = value; updateGraphics()
+		}
+
+	val graphics = graphics {
+	}
+
+	init {
+		updateGraphics()
+	}
+
+	private fun updateGraphics() {
+		graphics.apply {
+			clear()
+			fill(color) {
+				circle(0.0, 0.0, radius)
+			}
+		}
+	}
+}
+
