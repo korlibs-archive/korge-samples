6d640098db586429f3578ad5394f0fc0655febfc
soywiz <soywiz@gmail.com>
2019-11-29 23:01:04 +0100

    Use groovy instead of kotin-dsl to improve startup time

--- sample-input/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-import com.soywiz.korge.gradle.*
-
-apply(plugin = "korge")
-
-korge {
-	id = "com.soywiz.samples.input"
-}
--- settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-enableFeaturePreview("GRADLE_METADATA")
-
-for (file in rootDir.listFiles()) {
-	if (file.isDirectory && file.name.startsWith("sample-") && (File(file, "build.gradle").exists() || File(file, "build.gradle.kts").exists())) {
-		include(":${file.name}")
-	}
-}
