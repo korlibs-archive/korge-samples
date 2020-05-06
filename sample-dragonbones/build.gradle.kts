86bf8731c436ea9de03c2ef3efe1dd38fe282ce6
soywiz <soywiz@gmail.com>
2020-05-07 01:35:10 +0200

    Bump version to 1.12.4.0

--- sample-dragonbones/build.gradle.kts
+++ sample-dragonbones/build.gradle.kts
@@ -3,9 +3,11 @@ import com.soywiz.korge.gradle.*
 apply<KorgeGradlePlugin>()
 
 korge {
+	supportDragonbones()
+
 	id = "com.soywiz.korlibs.korge.samples.dragonbones"
 	name = "KorGE - DragonBones"
 	description = "KorGE sample using DragonBones plugin"
-	supportDragonbones()
+	orientation = Orientation.LANDSCAPE
 	icon = file("src/commonMain/resources/icon.png")
 }
