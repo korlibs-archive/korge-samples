1941cd58ed5eae2785291daac7e8281d8098bfee
Nicolai Emig <nicolai.emig@amseegym.de>
2020-05-14 09:20:58 +0200

    Added 10000 Sprite Sample

---
+++ sample-10000sprites/build.gradle.kts
@@ -0,0 +1,10 @@
+import com.soywiz.korge.gradle.*
+
+apply<KorgeGradlePlugin>()
+
+korge {
+	id = "com.soywiz.sample10000Sprites"
+	name = "SampleSpriteAnimationWith10000Sprites"
+	description = "A sample using Korge and the gradle plugin"
+	orientation = com.soywiz.korge.gradle.Orientation.LANDSCAPE
+}
---
+++ sample-10000sprites/src/commonMain/kotlin/Main.kt
@@ -0,0 +1,66 @@
+import com.soywiz.klock.*
+import com.soywiz.korev.*
+import com.soywiz.korge.*
+import com.soywiz.korge.input.*
+import com.soywiz.korge.view.*
+import com.soywiz.korim.bitmap.*
+import com.soywiz.korim.format.*
+import com.soywiz.korio.file.std.*
+
+suspend fun main() = Korge(width = 1600, height = 1200) {
+
+	val numberOfGreen = 5000
+	val numberOfRed = numberOfGreen
+
+	val redSpriteMap = resourcesVfs["gfx/character/character.png"].readBitmap()
+	val greenSpriteMap = resourcesVfs["gfx/character/character2.png"].readBitmap()
+
+	val greenAnimations = animations(greenSpriteMap)
+	val redAnimations = animations(redSpriteMap)
+
+	val greenSprites = Array<Sprite>(numberOfGreen) {
+		Sprite(greenAnimations[it % greenAnimations.size]).xy((10..1590).random(), (10..1190).random()).scale(2.0)
+	}
+
+	val redSprites = Array<Sprite>(numberOfRed) {
+		Sprite(redAnimations[it % redAnimations.size]).xy((10..1590).random(), (10..1190).random()).scale(2.0)
+	}
+
+	redSprites.forEach {
+		addChild(it)
+	}
+
+	greenSprites.forEach {
+		addChild(it)
+	}
+
+	addUpdater {
+
+		greenSprites.forEachIndexed { index, sprite ->
+			sprite.playAnimationLooped(animations(greenSpriteMap)[index % greenAnimations.size]).apply { walkdirection(sprite, index % greenAnimations.size) }
+		}
+
+		redSprites.forEachIndexed { index, sprite ->
+			sprite.playAnimationLooped(animations(redSpriteMap)[index % redAnimations.size]).apply { walkdirection(sprite, index % redAnimations.size) }
+		}
+
+	}
+}
+
+fun animations(spriteMap: Bitmap)  = arrayOf(
+	SpriteAnimation(spriteMap,16,32,96,1,4,1), // left
+	SpriteAnimation(spriteMap,16,32,32,1,4,1), // right
+	SpriteAnimation(spriteMap,16,32,64,1,4,1), // up
+	SpriteAnimation(spriteMap,16,32,0,1,4,1)) // down
+
+fun walkdirection(sprite : Sprite, indexOfAnimation : Int) {
+	sprite.apply {
+		val delta = 2
+		when (indexOfAnimation) {
+			0 -> x-=delta
+			1 -> x+=delta
+			2 -> y-=delta
+			3 -> y+=delta
+		}
+	}
+}
