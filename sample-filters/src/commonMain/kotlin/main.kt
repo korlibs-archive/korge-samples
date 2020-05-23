20de81273ca77e72f8b94b43819fb1533a1f1ff4
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-05-23 14:28:04 +0200

    Fixes sample-filters

--- sample-filters/src/commonMain/kotlin/main.kt
+++ sample-filters/src/commonMain/kotlin/main.kt
@@ -1,29 +1,29 @@
+import com.soywiz.klock.*
 import com.soywiz.korge.*
+import com.soywiz.korge.tween.*
 import com.soywiz.korge.view.*
-import com.soywiz.korim.bitmap.*
+import com.soywiz.korge.view.filter.*
 import com.soywiz.korim.color.*
-import com.soywiz.korim.font.*
-import com.soywiz.korim.vector.*
-import com.soywiz.korim.vector.paint.*
+import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
+import com.soywiz.korma.geom.*
+import com.soywiz.korma.interpolation.*
 
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
-	//val wave = WaveFilter()
-	val font = resourcesVfs["myfont-bug.ttf"].readTtfFont()
-	val bitmap = NativeImage(512, 128).context2d {
-		this.font = font
-		this.fontSize = 32.0
-		this.fillStyle = ColorPaint(Colors.RED)
-		this.verticalAlign = VerticalAlign.TOP
-		fillText("HELLO WORLD", x = 0.0, y = 0.0)
+	val minDegrees = (-16).degrees
+	val maxDegrees = (+16).degrees
+
+	val wave = WaveFilter()
+	val image = image(resourcesVfs["korge.png"].readBitmap()) {
+		rotation = maxDegrees
+		anchor(.5, .5)
+		scale(.8)
+		position(256, 256)
+		filter = wave
 	}
 
-	image(bitmap)
-
-	/*
 	while (true) {
 		image.tween(image::rotation[minDegrees], wave::time[1.seconds], time = 1.seconds, easing = Easing.EASE_IN_OUT)
 		image.tween(image::rotation[maxDegrees], wave::time[0.seconds], time = 1.seconds, easing = Easing.EASE_IN_OUT)
 	}
-	 */
 }
