2505caccfe588f61c04aa07562a853e719b3ec8d
RezMike <mihail.reznichenko.98@mail.ru>
2020-06-17 03:58:28 +0700

    Remove korio sample

--- korio/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-import com.soywiz.korge.gradle.*
-
-apply<KorgeGradlePlugin>()
-
-korge {
-	id = "com.soywiz.samples.korio"
-}
--- korio/src/commonMain/kotlin/main.kt
+++
@@ -1,6 +0,0 @@
-import com.soywiz.korio.*
-import com.soywiz.korio.file.std.*
-
-suspend fun main() = Korio {
-	println(resourcesVfs["hello.txt"].readString())
-}
