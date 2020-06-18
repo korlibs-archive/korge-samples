499bb7abf8e00eac1bca02a64d52497146f5e551
RezMike <mihail.reznichenko.98@mail.ru>
2020-06-18 23:13:46 +0700

    Stylish fixes in pong sample

--- pong/src/commonMain/kotlin/MenuScene.kt
+++ pong/src/commonMain/kotlin/MenuScene.kt
@@ -1,37 +1,33 @@
-import com.soywiz.korev.Key
-import com.soywiz.korge.input.keys
-import com.soywiz.korge.input.onClick
-import com.soywiz.korge.scene.Scene
-import com.soywiz.korge.view.Container
-import com.soywiz.korge.view.text
+import com.soywiz.korge.input.*
+import com.soywiz.korge.scene.*
 import com.soywiz.korge.ui.*
-import com.soywiz.korge.view.position
-import com.soywiz.korim.color.Colors
+import com.soywiz.korge.view.*
+import com.soywiz.korim.color.*
 
 class MenuScene() : Scene() {
-    suspend override fun Container.sceneInit() {
-        // set a background color
-        views.clearColor = Colors.BLACK
+	suspend override fun Container.sceneInit() {
+		// set a background color
+		views.clearColor = Colors.BLACK
 
-        // Add a text to show the name of the game
-        var gameNameText = text("Super Pong Bros II") {
-            position(views.virtualWidth/2 - 128, views.virtualHeight/2 - 128)
-        }
+		// Add a text to show the name of the game
+		var gameNameText = text("Super Pong Bros II") {
+			position(views.virtualWidth / 2 - 128, views.virtualHeight / 2 - 128)
+		}
 
-        var playButton = textButton(256.0, 32.0) {
-            text = "Play"
-            position(views.virtualWidth/2 - 128, views.virtualHeight/2 - 64)
-            onClick {
-                  sceneContainer.changeToAsync<PlayScene>()
-            }
-        }
-        var exitButton = textButton(256.0, 32.0) {
-            text = "Exit"
-            position(views.virtualWidth/2 - 128, views.virtualHeight/2)
-            onClick {
-                  views.gameWindow.close()
-            }
-        }
-    }
+		var playButton = textButton(256.0, 32.0) {
+			text = "Play"
+			position(views.virtualWidth / 2 - 128, views.virtualHeight / 2 - 64)
+			onClick {
+				sceneContainer.changeTo<PlayScene>()
+			}
+		}
+		var exitButton = textButton(256.0, 32.0) {
+			text = "Exit"
+			position(views.virtualWidth / 2 - 128, views.virtualHeight / 2)
+			onClick {
+				views.gameWindow.close()
+			}
+		}
+	}
 
 }
