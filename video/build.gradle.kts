a177986758dc35cb1a63416e36717685732d117f
soywiz <soywiz@gmail.com>
2020-06-24 01:24:03 +0200

    Some korvi adjustments

--- video/build.gradle.kts
+++ video/build.gradle.kts
@@ -10,5 +10,5 @@ korge {
 }
 
 dependencies {
-	add("commonMainApi", "com.soywiz.korlibs.korvi:korvi:0.2.1")
+	add("commonMainApi", "com.soywiz.korlibs.korvi:korvi:0.2.2-SNAPSHOT")
 }
--- video/src/commonMain/kotlin/main.kt
+++ video/src/commonMain/kotlin/main.kt
@@ -2,31 +2,45 @@ import com.soywiz.klock.*
 import com.soywiz.klock.hr.*
 import com.soywiz.korge.*
 import com.soywiz.korge.input.*
+import com.soywiz.korge.render.RenderContext
 import com.soywiz.korge.view.*
 import com.soywiz.korim.bitmap.*
 import com.soywiz.korim.color.*
 import com.soywiz.korio.async.*
 import com.soywiz.korio.file.*
 import com.soywiz.korio.file.std.*
+import com.soywiz.korio.lang.Cancellable
 import com.soywiz.korio.util.*
 import com.soywiz.korvi.*
+import kotlin.coroutines.coroutineContext
 
 suspend fun main() = Korge(width = 1280, height = 720, bgcolor = Colors["#2b2b2b"]) {
-	val view = korviView(views, resourcesVfs["video.mp4"])
-	if (OS.isJs) {
-		val text = text("Click to start playing the video...")
-		mouse.click.once {
-			text.removeFromParent()
+	//addUpdaterOnce {
+		val view = korviView(views, resourcesVfs["video.mp4"])
+		if (OS.isJs) {
+			val text = text("Click to start playing the video...")
+			mouse.click.once {
+				text.removeFromParent()
+				view.play()
+			}
+		} else {
 			view.play()
 		}
-	} else {
-		view.play()
+	//}
+}
+
+fun View.addUpdaterOnce(block: () -> Unit) {
+	var cancellable: Cancellable? = null
+	cancellable = addUpdater {
+		cancellable?.cancel()
+		block()
 	}
 }
 
 inline fun Container.korviView(views: Views, video: KorviVideo, callback: KorviView.() -> Unit = {}): KorviView = KorviView(views, video).also { addChild(it) }.also { callback(it) }
 suspend inline fun Container.korviView(views: Views, video: VfsFile, autoPlay: Boolean = true, callback: KorviView.() -> Unit = {}): KorviView = KorviView(views, video, autoPlay).also { addChild(it) }.also { callback(it) }
 class KorviView(val views: Views, val video: KorviVideo) : Image(Bitmaps.transparent), AsyncCloseable, BaseKorviSeekable by video {
+	val onPrepared = Signal<Unit>()
 	val onCompleted = Signal<Unit>()
 	var autoLoop = true
 
@@ -34,17 +48,38 @@ class KorviView(val views: Views, val video: KorviVideo) : Image(Bitmaps.transpa
 		suspend operator fun invoke(views: Views, file: VfsFile, autoPlay: Boolean = true): KorviView {
 			return KorviView(views, KorviVideo(file)).also {
 				if (autoPlay) {
-					it.video.play()
+					it.play()
 				}
 			}
 		}
+	}
+
+	private var _prepared: Boolean = false
+
+	private suspend fun ensurePrepared() {
+		if (!_prepared) {
+			onPrepared.waitOne()
+		}
+	}
+
+	override fun renderInternal(ctx: RenderContext) {
+		if (!_prepared) {
+			video.prepare()
+			_prepared = true
+			onPrepared()
+		} else {
+			video.render()
+		}
+		super.renderInternal(ctx)
 	}
 
 	val elapsedTime: TimeSpan get() = video.elapsedTime
 	val elapsedTimeHr: HRTimeSpan get() = video.elapsedTimeHr
 
 	fun play() {
+		if (video.running) return
 		views.launchImmediately {
+			ensurePrepared()
 			video.play()
 		}
 	}
@@ -54,7 +89,7 @@ class KorviView(val views: Views, val video: KorviVideo) : Image(Bitmaps.transpa
 	init {
 		video.onVideoFrame {
 			//println("VIDEO FRAME! : ${it.position.timeSpan},  ${it.duration.timeSpan}")
-			if (OS.isJs) {
+			if (OS.isJs || OS.isAndroid) {
 			//if (false) {
 				bitmap = it.data.slice()
 				//println(it.data)
