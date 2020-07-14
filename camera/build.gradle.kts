ee2a8dd6188b72d63e97efe4b146be62148e2ebc
RezMike <mihail.reznichenko.98@mail.ru>
2020-07-15 04:25:06 +0700

    Add camera sample, small improvement for shapes sample

---
+++ camera/build.gradle.kts
@@ -0,0 +1,16 @@
+import com.soywiz.korge.gradle.*
+
+apply<KorgeGradlePlugin>()
+
+korge {
+	id = "com.soywiz.sample"
+	name = "Camera sample"
+	description = "A sample using Camera in KorGE"
+	orientation = com.soywiz.korge.gradle.Orientation.LANDSCAPE
+	jvmMainClassName = "SampleKt"
+
+	supportShapeOps()
+	supportTriangulation()
+	supportDragonbones()
+	supportBox2d()
+}
