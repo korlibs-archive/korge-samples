051bd7b9cce8b81538e47c8b6e3c32ea352a2a05
soywiz <soywiz@gmail.com>
2020-11-11 15:03:56 +0100

    Atlas tests fix

--- atlas/src/commonTest/kotlin/test.kt
+++ atlas/src/commonTest/kotlin/test.kt
@@ -1,6 +1,6 @@
-import com.soywiz.korge.atlas.*
 import com.soywiz.korge.tests.*
 import com.soywiz.korge.view.*
+import com.soywiz.korim.atlas.*
 import com.soywiz.korim.bitmap.*
 import com.soywiz.korio.async.*
 import com.soywiz.korio.file.std.*
@@ -12,12 +12,12 @@ class AtlasTest : ViewsForTesting() {
 	fun test() = viewsTest {
 		atlasMain()
 		assertEquals(3, stage.numChildren)
-		assertEquals(Size(68, 204), (stage[0] as Image).texture.bmp.size)
+		assertEquals(Size(128, 256), (stage[0] as Image).bitmap.bmp.size)
 	}
 
 	@Test
 	fun testAtlas() = suspendTest {
-		val atlas = resourcesVfs["logos.atlas.json"].readAtlas(views)
+		val atlas = resourcesVfs["logos.atlas.json"].readAtlas()
 		assertEquals(3, atlas.entries.size)
 		assertEquals(Size(64, 64), atlas["korau.png"].size)
 		assertEquals(Size(64, 64), atlas["korge.png"].size)
