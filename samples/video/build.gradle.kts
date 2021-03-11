bf5fbbae743c36111d24bf138d1238888822639f
soywiz <soywiz@gmail.com>
2021-03-11 20:28:18 +0100

    Use korvi from central

--- samples/video/build.gradle.kts
+++ samples/video/build.gradle.kts
@@ -11,10 +11,6 @@ korge {
 	targetDefault()
 }
 
-repositories {
-	maven { url = uri("http://dl.bintray.com/korlibs/korlibs/") }
-}
-
 dependencies {
-	add("commonMainApi", "com.soywiz.korlibs.korvi:korvi:2.0.5")
+	add("commonMainApi", "com.soywiz.korlibs.korvi:korvi:2.0.7")
 }
