e586bf5d0efb724ed35bf0999f463fdbc432adab
Carlos Ballesteros Velasco <soywiz@gmail.com>
2021-07-29 06:51:36 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ buildscript {
 
 	repositories {
 		mavenLocal()
-		maven { url = uri("https://plugins.gradle.org/m2/") }
 		mavenCentral()
 		google()
+		maven { url = uri("https://plugins.gradle.org/m2/") }
 	}
 	dependencies {
 		classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
